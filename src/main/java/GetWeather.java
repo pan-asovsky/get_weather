0107d15b1e53f2e7a192ea868f80546771de6b0d
Damian P <panosovski@gmail.com>
2021-11-12 22:59:06 +0300

    The code is divided into methods. The variable initiation option has been changed. Adjusted the overall output.

--- src/main/java/GetWeather.java
+++ src/main/java/GetWeather.java
@@ -1,78 +1,61 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
+import java.io.IOException;
+
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.DecimalFormat;
-import java.util.HashMap;
+import java.net.MalformedURLException;
+
 import java.util.List;
 import java.util.Map;
-
+import java.util.HashMap;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
 
 public class GetWeather {
 
     public static Map<String, Object> jsonToMap(String str) {
         return new Gson().fromJson
-                (str, new TypeToken<HashMap<String, Object>>() {}.getType());
+                (str, new TypeToken<HashMap<String, Object>>(){}
+                        .getType());
     }
 
+    //TODO Изменить параметры функции для вызова из консоли с переданным значением.
     public static void main(String[] args) {
 
+        String LOCATION = getLocation();
         String API_KEY = "6df5fdbc30838dd60a6e5cbe8f75ebf9";
-        String LOCATION = "";
-
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Choice your city: ");
-            LOCATION = reader.readLine();
-        } catch (IOException e) {
-            System.out.println("Wrong city or input!");
-            e.printStackTrace();
-        }
-
         String urlString = "https://api.openweathermap.org/data/2.5/weather?q="
                 + LOCATION + "&appid=" + API_KEY + "&units=metric";
 
         try {
-            StringBuilder result = new StringBuilder();
-            URL url = new URL(urlString);
-            URLConnection connection = url.openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
-
-            //System.out.println(result);
-
-            Map<String, Object> responseMap = jsonToMap(result.toString());
+            Map<String, Object> responseMap = jsonToMap(getConnectionData(urlString));
             Map<String, Object> mainInfo = jsonToMap(responseMap.get("main").toString());
             Map<String, Object> windInfo = jsonToMap(responseMap.get("wind").toString());
 
             @SuppressWarnings("unchecked")
-            List<Map<String, Object>> weatherInfoList = (List<Map<String, Object>>)(responseMap.get("weather"));
+            List<Map<String, Object>> weatherInfoList = (List<Map<String, Object>>) (responseMap.get("weather"));
             Map<String, Object> weatherInfo = weatherInfoList.get(0);
 
-
-            String weatherDescription = String.valueOf(weatherInfo.get("main"));
-            int humidity = Integer.parseInt(new DecimalFormat("#").format(mainInfo.get("humidity")));
-            int temperature = Integer.parseInt(new DecimalFormat("#").format(mainInfo.get("temp")));
-            int temperatureFeels = Integer.parseInt(new DecimalFormat("#").format(mainInfo.get("feels_like")));
-            double windSpeed = Double.parseDouble(new DecimalFormat("#").format(windInfo.get("speed")));
-            String windDirection = getWindDirection(windInfo.get("deg"));
+            String weatherDescription = weatherInfo.get("description").toString();
+            int temperature = getInt(mainInfo.get("temp"));
+            int temperatureFeels = getInt(mainInfo.get("feels_like"));
+            int humidity = getInt(mainInfo.get("humidity"));
+            int pressure = getPressure(getInt(mainInfo.get("pressure")));
+            double windSpeed = getDouble(windInfo.get("speed"));
+            double windGust = getDouble(windInfo.get("gust"));
+            String windDirection = getWindDirection(getDouble(windInfo.get("deg")));
 
             System.out.println(
-                    "\n" + LOCATION + ", " +
-                    weatherDescription +
-                    "\nTemperature: " + temperature + "°C\n" +
-                    "Feels_like: " + temperatureFeels + "°C\n" +
-                    "Humidity: " + humidity + "%" + "\n"  +
-                    "Wind: " + windDirection + ", " + windSpeed + " m/s\n"
+                    "\nIn " + LOCATION + " is " +
+                            weatherDescription +
+                            "\nTemperature " + temperature + "°C, " +
+                            "feels like " + temperatureFeels + "°C\n" +
+                            "Humidity " + humidity + "%, pressure " + pressure + " mm\n" +
+                            "Wind " + windDirection + ", " + windSpeed +
+                            " m/s, gusts up to " + windGust + "m/s\n"
             );
 
         } catch (MalformedURLException e) {
@@ -82,12 +65,50 @@ public class GetWeather {
         }
     }
 
-    public static String getWindDirection(Object angle) {
+    public static String getConnectionData(String address) throws IOException {
 
-        double windAngle = Double.parseDouble(new DecimalFormat("#").format((Number) angle));
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(address);
+        URLConnection connection = url.openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
+    }
+
+    public static double getDouble(Object obj) {
+
+        double result = Double.parseDouble(obj.toString());
+        return Math.ceil(result * 10) / 10;
+    }
+
+    public static int getInt(Object obj) {
+        return (int) Math.round(Double.parseDouble(obj.toString()));
+    }
+
+    public static String getWindDirection(double angle) {
+
         String[] windDirections = {"northern", "northeast", "eastern",
                 "southeast", "south", "southwest", "western", "northwest"};
+        return windDirections[(int) Math.floor((angle % 360) / 45)];
+    }
 
-        return windDirections[(int)Math.floor((windAngle % 360) / 45)];
+    public static String getLocation() {
+
+        String userLocation = "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("Enter your city: ");
+            userLocation = reader.readLine();
+        } catch (IOException e) {
+            e.getMessage();
+        }
+        return userLocation;
+    }
+
+    public static int getPressure(int pressure) {
+        return (int) Math.round(pressure * 0.75);
     }
 }
